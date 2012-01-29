3bf09244795e2e0ea9992438a180b5bb21b57c1d
Andrey M0shbear <andrey@moshbear.net>
2012-01-28 20:47:33 -0500

    Beta 2.
    New features:
    - console logging
    - chunk size changed from 50k to 500k
    - array_to_string now does hex strings
    - messagebox popup on completion

--- Downloader/Form1.cs
+++ Downloader/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -32,6 +33,26 @@ namespace Downloader
         private Link[] links;
 
         private bool hasError;
+
+        private static string array_to_string<T>(T[] array)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T x in array)
+            {
+                sb.Append(String.Format("{0:X}", x));
+            }
+            return sb.ToString();
+        }
+
+        private static bool array_eq<T>(T[] a1, T[] a2)
+        {
+            if (a1.Length != a2.Length)
+                return false;
+            for (int i = 0; i < a1.Length; ++i)
+                if (!a1[i].Equals(a2[i]))
+                    return false;
+            return true;
+        }
 
         // mkdir -p
         // first path component of path0 must be valid or
@@ -80,11 +101,14 @@ namespace Downloader
 
         private static bool matchesSize(string fname, long sz)
         {
-            return ((new FileInfo(fname)).Length == sz);
+            long fsz = (new FileInfo(fname)).Length;
+            return (fsz == sz);
         }
         private bool matchesHash(string fname, byte[] h)
         {
-            return hashFile(fname).Equals(h);
+            byte[] x = hashFile(fname);
+            bool z = array_eq(x, h);
+            return z;
         }
 
         // Tokenize sha1:size:link\n to array of sha-link pairs
@@ -97,30 +121,32 @@ namespace Downloader
             }
             string[] lines = file_data.Split('\n');
             root = lines[0];
-            links = new Link[lines.Length - 1];
+            ArrayList al = new ArrayList();
             for (int i = 1; i < lines.Length; ++i)
             {
+                if (lines[i].Length == 0 || lines[i][0] == '#')
+                    continue;
                 string[] comp = lines[i].Split(':');
                 if (comp.Length != 3)
                 {
                     MessageBox.Show("Tokenizer error: input lines does not split into two tokens");
-                    hasError = true;
-                    return;
+                    continue;
                 }
                 byte[] b = new byte[comp[0].Length / 2];
                 for (int j = 0; j < comp[0].Length; ++j)
                 {
                     if (j % 2 == 0)
-                        b[j / 2] = Byte.Parse(comp[0].Substring(j, 2));
+                        b[j / 2] = Byte.Parse(comp[0].Substring(j, 2),
+                                    System.Globalization.NumberStyles.HexNumber);
                 }
-
-                links[i - 1] = new Link
-                {
-                    hash = b,
-                    size = long.Parse(comp[1]),
-                    path = comp[2]
-                };
+                al.Add(new Link
+                        {
+                            hash = b,
+                            size = long.Parse(comp[1]),
+                            path = comp[2]
+                        });
             }
+            links = (Link[])al.ToArray(typeof(Link));
         }
 
 
@@ -178,6 +204,7 @@ namespace Downloader
                         }
                     } while (count > 0);
                     file_data = sb.ToString();
+                    MessageBox.Show("Download OK");
                 }
             }
             catch (WebException we)
@@ -213,12 +240,33 @@ namespace Downloader
             {
                 string localFile = (new StringBuilder(destPathTxt.Text))
                     .Append('/').Append(link.path).ToString().Replace('\\', '/');
+                progressTxt.Text += localFile + ":";
+                Console.Write(localFile + ":");
                 // Check if file is ok before continuing
                 if (File.Exists(localFile)) {
                     // Check size first (and then hash if match) for speed
                     if (matchesSize(localFile, link.size)) {
-                       if (matchesHash(localFile, link.hash))
-                               continue;
+                        progressTxt.Text += String.Format(" size ok ({0})", link.size);
+                        Console.Write(" size ok ({0})", link.size);
+                        byte[] fhash = hashFile(localFile);
+                        if (array_eq(fhash, link.hash))
+                        {
+                            progressTxt.Text += String.Format(", hash ok ({0})\r\n", array_to_string(link.hash));
+                            Console.Write(", hash ok ({0})\r\n", array_to_string(link.hash));
+                            continue;
+                        }
+                        else
+                        {
+                            progressTxt.Text += String.Format(", HASH FAIL(got: {0}, expected: {1};", array_to_string(fhash), array_to_string(link.hash));
+                            Console.Write(", HASH FAIL(got: {0}, expected: {1};", array_to_string(fhash), array_to_string(link.hash));
+                        }
+                    }
+                    else
+                    {
+                        progressTxt.Text += String.Format(" SIZE FAIL (got: {0}, expected: {1}; ",
+                                    (new FileInfo(localFile)).Length, link.size);
+                        Console.Write(" SIZE FAIL (got: {0}, expected: {1}; ",
+                                    (new FileInfo(localFile)).Length, link.size);
                     }
                 }
                 string localFolder = localFile.Substring(0, localFile.LastIndexOf('/'));
@@ -226,6 +274,7 @@ namespace Downloader
 
                 string remotePath = (new StringBuilder(root)).Append('/').Append(link.path).ToString();
                 progressTxt.Text += "File not found or fails check: downloading:";
+                Console.Write("File not found or fails check: downloading:");
                 // File download
                 try
                 {
@@ -235,14 +284,15 @@ namespace Downloader
                     {
                         if (wr.StatusCode != HttpStatusCode.OK)
                         {
-                            progressTxt.Text += String.Format(" HTTP ERROR: {0}\n", wr.StatusCode);
+                            progressTxt.Text += String.Format(" HTTP ERROR: {0}\r\n", wr.StatusCode);
+                            Console.Write(" HTTP ERROR: {0}\r\n", wr.StatusCode);
                             continue;
                         }
                         using (BinaryWriter bw =
                                 new BinaryWriter(File.Open(localFile, FileMode.OpenOrCreate)))
                         {
                             Stream r = wr.GetResponseStream();
-                            byte[] buf = new byte[50000]; // download in 50k chunks
+                            byte[] buf = new byte[500000]; // download in 500k chunks
                             int count;
                             do
                             {
@@ -251,9 +301,15 @@ namespace Downloader
                                 {
                                     bw.Write(buf, 0, count);
                                     if (count == buf.Length)
+                                    {
                                         progressTxt.Text += "@";
+                                        Console.Write('@');
+                                    }
                                     else
+                                    {
                                         progressTxt.Text += "$";
+                                        Console.Write('$');
+                                    }
                                 }
                             } while (count > 0);
                         }
@@ -261,27 +317,35 @@ namespace Downloader
                 }
                 catch (WebException we)
                 {
-                    progressTxt.Text += String.Format(" WE ERROR: {0}\n", we.Message);
+                    progressTxt.Text += String.Format(" WEB ERROR: {0}\r\n", we.Message);
+                    Console.Write(" WEB ERROR: {0}\r\n", we.Message);
                     continue;
                 }
                 // check if download went ok
                 if (matchesSize(localFile, link.size))
                 {
                     progressTxt.Text += String.Format(" size ok ({0})", link.size);
+                    Console.Write(" size ok ({0})", link.size);
                     byte[] fhash = hashFile(localFile);
-                    if (fhash.Equals(link.hash))
+                    if (array_eq(fhash, link.hash))
                     {
-                        progressTxt.Text += String.Format(", hash ok ({0})\n", link.hash);
+                        progressTxt.Text += String.Format(", hash ok ({0})\r\n", array_to_string(link.hash));
+                        Console.Write(", hash ok ({0})\r\n", array_to_string(link.hash));
                     }
                     else
                     {
-                        progressTxt.Text += String.Format(", HASH FAIL(got: {0}, expected: {1}\n", fhash, link.hash);
+                        progressTxt.Text += String.Format(", HASH FAIL(got: {0}, expected: {1}\r\n", array_to_string(fhash), array_to_string(link.hash));
+                        Console.Write(", HASH FAIL(got: {0}, expected: {1}\r\n", array_to_string(fhash), array_to_string(link.hash));
                     }
                 } else {
-                    progressTxt.Text += String.Format(" SIZE FAIL (got: {0}, expected: {1}\n",
+                    progressTxt.Text += String.Format(" SIZE FAIL (got: {0}, expected: {1}\r\n",
+                                (new FileInfo(localFile)).Length, link.size);
+                    Console.Write(" SIZE FAIL (got: {0}, expected: {1}\r\n",
                                 (new FileInfo(localFile)).Length, link.size);
                 }
             }
+            Console.WriteLine("done");
+            MessageBox.Show("Downloading complete.");
         }
 
         private void selectDestBtn_Click(object sender, EventArgs e)
